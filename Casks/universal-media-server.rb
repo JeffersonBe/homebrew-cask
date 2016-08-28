cab2559293f3287836b0401bd0b8a4bcedd9dd77
Jefferson Bonnaire <jefferson.bonnaire@gmail.com>
2016-08-28 15:11:43 +0100

    Upgrade Universal Media Server to 6.5.0

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,11 +1,11 @@
 cask 'universal-media-server' do
-  version '5.3.0'
-  sha256 '13dec722c7b85cf2e193639d15b37f11eda2fb7f17d02e33b28e06d67ecd94cc'
+  version '6.5.0'
+  sha256 'c5e1042b828029933582bda6e14bf7e1bce44a0f7e101cb38a9673ed7d089f9d'
 
   # sourceforge.net/unimediaserver was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}-Java8.dmg"
   appcast 'https://sourceforge.net/projects/unimediaserver/rss?path=/Official%20Releases',
-          checkpoint: 'd84d75d92afff92462b5f39efdabacbdfd82ae3d70c7de130d32a43a3f38317d'
+          checkpoint: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   name 'Universal Media Server'
   homepage 'http://universalmediaserver.com'
   license :gpl
